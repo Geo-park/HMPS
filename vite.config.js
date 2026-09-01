f5b071a8dd98ffad8cb49ed55a2a4778018fc112
Geo-park <subagjaamar8@gmail.com>
2026-09-01 23:14:23 +0700

    favicon

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'favicon-cropped.png') {
-            return 'favicon-cropped.png';
-          }
-          return '[name]-[hash][extname]';
-        },
-      },
-    },
+    copyPublicDir: true,
   },
 })
